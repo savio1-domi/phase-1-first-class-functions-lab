d46ed5e6d5788fee952867b04f32548f52cc2016
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 19:57:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
-
+/* Returns an array containing the first two 
+   drivers from the passed-in array */
 const returnFirstTwoDrivers = (drivers)=>{
     const newArr = []
     let l1 = `${drivers[0]}`
@@ -10,7 +11,7 @@ const returnFirstTwoDrivers = (drivers)=>{
 }
 returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
 
-
+// Returns an array of the last two drivers
 const returnLastTwoDrivers = (drivers)=>{
     const newArr = []
     let l1 = `${drivers[2]}`
@@ -21,21 +22,23 @@ const returnLastTwoDrivers = (drivers)=>{
 }
 returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
 
-
+// Allows to invoke either function from the array
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-
+/* Returns a function and multiply a given value
+using the created multiplier*/
 const createFareMultiplier = function (fareMultiplier) {
     return function(cost) {
         return fareMultiplier * cost;
       }
 }
-
+// A function doubles fares
 const fareDoubler = createFareMultiplier(2);
+// a function tripples fares
 const fareTripler = createFareMultiplier(3);
+
 
 function selectDifferentDrivers(drivers, drive) {
   return drive(drivers);
 }
 
-console.log(createFareMultiplier()())
